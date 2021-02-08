d5ec76875b831fe048486c863c271f130bf85bf1
Francisco Amador <developer.ti2@mexq.com.mx>
2021-02-08 10:24:58 -0600

    Se agrego la página de inicio. Se muestra la fecha y la zona horaria

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PHP EXAMPLE</title>
+</head>
+<body>
+  <h1>Fecha</h1>
+  <h2><?= date('Y-m-d H:i:s') ?></h2>
+  <h1>Zona horaraia</h1>
+  <h2><?= date_default_timezone_get() ?></h2>
+</body>
+</html>
